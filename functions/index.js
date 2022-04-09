b39cd55d640cf695090cdf7aef8d7cd5d17ae4e9
malikzeros <malikzeros@gmail.com>
2022-04-10 00:13:15 +0700

    for tutorial youtube

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,44 @@
 const functions = require("firebase-functions");
-
+const express = require("express");
+const admin = require("firebase-admin");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+// const serviceAccount = require("../abdmalikdev.json");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const app = express();
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+app.get("/data", (req, res) => {
+  db.settings({
+    timestampsInSnapshots: true,
+  });
+  const allData = [];
+  db.collection("project").
+      orderBy("Name")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((hasil) => {
+          allData.push(hasil.data());
+        });
+        // console.log(allData);
+        res.send(allData);
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
 });
+// app.post("/data", (req, res) => {
+//   db.settings({
+//     timestampsInSnapshots: true,
+//   });
+//   db.collection("project").add({
+//     nama: req.body.Name,
+//     Description: req.body.Description,
+//   });
+//   res.send({
+//     nama: req.body.Name,
+//     Description: req.body.Description,
+//   });
+// });
+exports.api = functions.https.onRequest(app);
